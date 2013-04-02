a1c3838ff4118bfe09c901017b80b2ce0c3c8b30
Micha Hernandez van Leuffen <mies@wercker.com>
2013-04-02 15:34:33 +0200

    let this test fail

--- app_test.py
+++ app_test.py
@@ -7,9 +7,9 @@ class StunticonTestCase(unittest.TestCase):
 
   def test_index(self):
     tester = app.test_client(self)
-    response = tester.get('/stunticons.json', content_type='application/json')
+    response = tester.get('/cities.json', content_type='application/json')
     self.assertEqual(response.status_code, 200)
-    self.assertEqual(response.data, json.dumps(['Amsterdam', 'San Francisco', 'Berlin', 'New York']))
+    self.assertEqual(response.data, json.dumps(['Amsterdam', 'San Francisco', 'Berlin', 'New Yorki', 'Paris']))
 
 if __name__ == '__main__':
     unittest.main()
